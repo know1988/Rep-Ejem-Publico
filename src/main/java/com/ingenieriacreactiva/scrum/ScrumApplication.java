58d580cd49bbfdd0afb547d36a21f9b1e9ef1701
JUAN DIEGO CANO MEJIA <know1988@hotmail.com>
2022-09-01 12:49:46 -0500

    Actualizacion

--- src/main/java/com/ingenieriacreactiva/scrum/ScrumApplication.java
+++ src/main/java/com/ingenieriacreactiva/scrum/ScrumApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KikeanaApplication {
+public class ScrumApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(KikeanaApplication.class, args);
+		SpringApplication.run(ScrumApplication.class, args);
 	}
 
 }
